8a16363e3d2c29ea772de41f8192c759e7815327
incognito1025 <nkhanamart@gmail.com>
2024-07-22 03:48:39 -0400

    added queries folder and queries/song.js file w/ getAllSongs function

---
+++ queries/song.js
@@ -0,0 +1,21 @@
+const db = require("../db/dbConfig.js");
+
+//sql query
+
+const getAllSongs = async () => {
+    try {
+      const allSongs = await db.any("SELECT * FROM songs");
+      return allSongss;
+    } catch (error) {
+      return error;
+    }
+  };
+
+
+module.exports = { getAllSongs };
+
+/*
+
+.any() will accept any return from the database, no rows, one row, or many rows of data.
+
+*/
