126f6b1fdc83af78e549df5a82e9d3fa90ee46d9
incognito1025 <nkhanamart@gmail.com>
2024-07-22 04:23:02 -0400

    added Index route ("/") in songsController.js for all songs

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const songs = express.Router();
+const { getAllSongs } = require("../queries/song");
 
 //INDEX
-songs.get("/", (req, res) => {
-    res.json({ status: "ok" });
+songs.get("/", async (req, res) => {
+    const allSongs = await getAllSongs();
+    if (allSongs[0]) {
+        res.status(200).json(allSongs);
+    } else {
+        res.status(500).json({ error: "server error" });
+    }
 });
 
 
