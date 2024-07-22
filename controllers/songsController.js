7f89488d7dca653eaa33c9f49fe293ac12b2af66
incognito1025 <nkhanamart@gmail.com>
2024-07-22 05:25:00 -0400

    added Show route ("/:id") in songsController.js to get one song by id

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/song");
+const { getAllSongs, getSong } = require("../queries/song");
 
 //INDEX
 songs.get("/", async (req, res) => {
@@ -9,6 +9,18 @@ songs.get("/", async (req, res) => {
         res.status(200).json(allSongs);
     } else {
         res.status(500).json({ error: "server error" });
+    }
+});
+
+
+//SHOW
+songs.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const song = await getSong(id);
+    if (song) {
+        res.json(song);
+    } else {
+        res.status(404).json({error: "not found"});
     }
 });
 
