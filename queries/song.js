960a09df04b137a335c6914c4eca21384bd46f74
incognito1025 <nkhanamart@gmail.com>
2024-07-22 05:09:36 -0400

    added function getSong in queries/song.js to get one song by id

--- queries/song.js
+++ queries/song.js
@@ -1,7 +1,6 @@
 const db = require("../db/dbConfig.js");
 
-//sql query
-
+//sql query for ALL songs
 const getAllSongs = async () => {
     try {
       const allSongs = await db.any("SELECT * FROM songs");
@@ -11,11 +10,33 @@ const getAllSongs = async () => {
     }
   };
 
+  //ONE song
+  const getSong = async (id) => {
+    try {
+        const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id);
+        return oneSong;
+    } catch (error) {
+        return error;
+    }
+  };
 
-module.exports = { getAllSongs };
+
+
+
+module.exports = { 
+    getAllSongs,
+    getSong,
+};
 
 /*
 
 .any() will accept any return from the database, no rows, one row, or many rows of data.
 
+//alternate to: 
+        const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id);
+        ```
+        await db.one("SELECT * FROM colors WHERE id=$[id]", {
+  id: id,
+});
+        ```
 */
